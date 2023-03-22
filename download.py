fe54ec68584841616efe4af55c528c66f88107a3
yannaingsmu <106371608+yannaingsmu@users.noreply.github.com>
2023-03-23 03:07:59 +0800

    Add files via upload

---
+++ download.py
@@ -0,0 +1,21 @@
+import requests
+import pandas as pd
+
+def download(url,dest):
+    #url = 'https://dl.djicdn.com/downloads/inspire_1/WM610_FW_V01.07.00.90.zip'
+    r = requests.get(url, allow_redirects=True)
+
+    open('./downloads/'+dest, 'wb').write(r.content)
+
+
+
+data = pd.read_csv('./supported_firmwares_xv4.csv')
+link = data.download_link.tolist()
+name = data.download_name.tolist()
+
+for x,y in zip(link,name):
+   
+    if not str(x) == 'nan':
+        print(x , " ***** ", type(x))
+        download(x,y)
+        print(str(y)+" done.")
